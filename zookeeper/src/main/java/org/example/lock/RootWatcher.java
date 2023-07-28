256d655a3f4513b182640151e1fa112ccf805639
ats1999 <rkt82224@gmail.com>
2023-07-29 00:14:54 +0530

    distributed lock watcher

---
+++ zookeeper/src/main/java/org/example/lock/RootWatcher.java
@@ -0,0 +1,11 @@
+package org.example.lock;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class RootWatcher implements Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+}
